5abf0950796823794cc372c1df6f9695a5c1adba
charleytrigano <charley@trigano.org>
2026-01-11 19:16:08 +0100

    Update style.py

--- utils/style.py
+++ utils/style.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+def apply_style():
+    st.markdown("""
+        <style>
+        body {
+            font-family: 'Segoe UI', sans-serif;
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
